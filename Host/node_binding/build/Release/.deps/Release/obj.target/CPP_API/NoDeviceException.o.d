8f914aa826ec73fbe133d1306183cf01edfe35c2
Denis Benato <benato.denis96@gmail.com>
2016-12-03 15:29:04 +0100

    No bin files, please

--- Host/node_binding/build/Release/.deps/Release/obj.target/CPP_API/NoDeviceException.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/DataChan/../CPP_API/NoDeviceException.o := g++ '-DNODE_GYP_MODULE_NAME=DataChan' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/denis/.node-gyp/6.9.1/include/node -I/home/denis/.node-gyp/6.9.1/src -I/home/denis/.node-gyp/6.9.1/deps/uv/include -I/home/denis/.node-gyp/6.9.1/deps/v8/include -I../../../../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/DataChan/../CPP_API/NoDeviceException.o.d.raw   -c -o Release/obj.target/DataChan/../CPP_API/NoDeviceException.o ../../CPP_API/NoDeviceException.cpp
-Release/obj.target/DataChan/../CPP_API/NoDeviceException.o: \
- ../../CPP_API/NoDeviceException.cpp ../../CPP_API/NoDeviceException.hpp
-../../CPP_API/NoDeviceException.cpp:
-../../CPP_API/NoDeviceException.hpp:
