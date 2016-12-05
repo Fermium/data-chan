ccda11a74146008540ca8eac83735cf5d92cc3cd
Davide Bortolami <d@fermiumlabs.com>
2016-12-05 19:13:26 +0100

    Trusty not xenial, nodejs 6, typos

--- setup_env.sh
+++ setup_env.sh
@@ -1,20 +1,20 @@
-echo updating apt
+echo "updating apt"
 apt-get update
 
-echo installing basic software
+echo "Installing basic software"
 apt-get install -y pkg-config  wget  curl git unzip tar unzip
 
-echo installing basic building software
+echo "Installing basic building software"
 apt-get install -y make autogen autoconf build-essential #general building utils
 apt-get install -y gcc g++ #default compilers
 
-echo installing avr software
+echo "Installing avr software"
 apt-get install -y gcc-avr avr-libc binutils-avr avrdude #avr compiler
 
-echo installing needed libs
+echo "Installing needed libs"
 apt-get install -y libusb-1.0-0-dev zlib1g-dev #libs
 
-echo installing swig
+echo "Installing swig"
 apt-get install -y byacc flex #necessary to build swig
 apt-get install -y libpcre3 libpcre3-dev #perl regex
 git clone https://github.com/arfoll/swig.git
@@ -24,12 +24,11 @@ chmod +x autogen.sh && ./autogen.sh
 make && make install
 cd ../ && rm -rf swig
 
-echo installing documentation software
+echo "Installing documentation software"
 apt-get install -y doxygen
 
-echo installing secondary software
+echo "Installing secondary software"
 apt-get install -y openssl libssl-dev openssh-client
 
-echo installing python
+echo "Installing python"
 apt-get install -y python python-dev #python and the library to build the binding API
-
