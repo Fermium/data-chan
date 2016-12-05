974319fc58940edee972cc68d2544d09a79bc5f6
Denis Benato <benato.denis96@gmail.com>
2016-12-05 16:43:17 +0100

    please travis, start working!

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+apt-get install -y byacc flex #necessary to build swig
+apt-get install -y libpcre3 libpcre3-dev #perl regex
+apt-get install -y openssl libssl-dev openssh-client
+apt-get install -y pkg-config  wget  curl git unzip tar unzip
+apt-get install -y libusb-1.0-0-dev zlib1g-dev #libs
+apt-get install -y python python-dev #python and the library to build the binding API
+apt-get install -y gcc g++ #default compilers
+apt-get install -y gcc-avr avr-libc binutils-avr #avr compiler
+apt-get install -y make autogen autoconf build-essential #general building utils
