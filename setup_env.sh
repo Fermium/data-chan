aa2a277f4ce8ca534cce1138b281fdedc25faad2
Denis Benato <benato.denis96@gmail.com>
2016-12-05 17:56:03 +0100

    Better environment script

--- setup_env.sh
+++ setup_env.sh
@@ -23,11 +23,21 @@ make && make install
 cd ../ && rm -rf swig
 
 echo installing documentation software
-apt-get install doxygen
+apt-get install -y doxygen
 
 echo installing secondary software
 apt-get install -y openssl libssl-dev openssh-client
 
 echo installing python
 apt-get install -y python python-dev #python and the library to build the binding API
+
+echo installing node.js
+export NODE_VER=6.9.1
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get install -y nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install $NODE_VER && nvm alias default $NODE_VER nvm use default
 
