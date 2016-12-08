01a3feed13f289609f63f8ead896ae2dc3081fc0
Denis Benato <benato.denis96@gmail.com>
2016-12-08 19:42:41 +0100

    Better shared lib building mechanism

--- flow.mk
+++ flow.mk
@@ -1,8 +1,28 @@
-CC = clang
-CXX = clang++
+CC	= clang
+CXX	= clang++
 
-PYTHON_CC = gcc
-PYTHON_FLAGS = -fPIC
+PYTHON_CC		= gcc
+PYTHON_FLAGS	= -fPIC
+PYTHON_LIB		= -lpython2.7
+PYTHON_INCLUDE	= -I/usr/include/python2.7
 
-CFLAGS = -Wall -fPIC -std=gnu99
-CXXFLAGS = -Wall -fPIC
+CFLAGS		= -Wall -fPIC -std=gnu99
+CXXFLAGS	= -Wall -fPIC
+
+SHARED_LIB_EXT	= .so
+SHARED_LIB_FLAG	= -shared
+
+
+SHARED_LIB_FLAG	= -shared
+ifeq ($(OS),Windows_NT)
+	SHARED_LIB_EXT := dll
+else
+	UNAME := $(shell uname)
+	ifeq ($(UNAME), Linux)
+		SHARED_LIB_EXT := so
+	endif
+	ifeq ($(UNAME), Darwin)
+		SHARED_LIB_FLAG = -dynamiclib
+		SHARED_LIB_EXT := dylib
+	endif
+endif
