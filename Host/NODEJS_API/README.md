51a43bccea2646654d2f0cf05dcb2120fcde9a74
Davide Bortolami <d@fermiumlabs.com>
2017-03-20 18:15:40 +0100

    new python script to upload to s3. trigger wercker

--- Host/NODEJS_API/README.md
+++ Host/NODEJS_API/README.md
@@ -1,1 +1,17 @@
 # Data-chan NodeJS FFI library
+
+Since Data-chan is a multi-platform piece of software the NodeJS library needs to be as well.
+
+To achieve that, Data-chan is automatically built to an amazon s3 bucket: 
+
+* libDataChan.so for Linux
+* libDataChan.dll for Windows
+* libDataChan.dylib for MacOS
+
+You need to set the following environmental variables for the script to run:
+
+* AWS_ACCESS_KEY_ID
+* AWS_SECRET_ACCESS_KEY
+* AWS_DESTINATION_BUCKET (defaults to "data-chan-js-binaries")
+* WERCKER_TOKEN
+* WERCKER_DESTINATION_PIPELINE (defaults to 5855662ab7a7370100caf8fd) 
---
+++ scripts/trigger-wercker.py
@@ -0,0 +1,41 @@
+import git
+import tinys3
+import glob
+import os
+import requests
+import json
+
+# Get the current repository commit hash
+repo = git.Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
+branch =  repo.active_branch.name
+print("SHA of this commit is " + sha + " on branch " + branch)
+
+# Get the file to upload
+fileToUpload = glob.glob('Host/libDataChan.*')[0]
+fileToUploadName = fileToUpload.replace("Host/", "")
+
+print("The file \"" + fileToUploadName +  "\" will be uploaded to s3")
+
+
+# Upload the datachan library to Amazon S3
+s3conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+f = open(fileToUpload,'rb')
+bucket = os.environ.get('AWS_DESTINATION_BUCKET', "data-chan-js-binaries") 
+s3conn.upload(sha + "/" + fileToUploadName,f, bucket )
+
+
+#if we are not on wercker
+if os.environ.get('WERCKER_MAIN_PIPELINE_STARTED', "") == "": 
+    print("We're not on Wercker, Triggering a new Wercker build")
+    url = 'https://app.wercker.com/api/v3/runs'
+    headers = {'Authorization':'Bearer ' + os.environ['WERCKER_TOKEN'] }
+    a = {"pipelineId": os.environ.get('WERCKER_DESTINATION_PIPELINE', "5855662ab7a7370100caf8fd"), 
+        "branch": branch, 
+        "commitHash": sha }
+    r = requests.get(url, headers=headers, verify=True, params=a)
+else:
+    print("We're running on Wercker. Skip triggering a new Wercker build")
+
+    
+    
