4ce939db5a75233e6f0b6f447c38c6b6d44d2747
Denis Benato <benato.denis96@gmail.com>
2017-03-20 19:32:18 +0100

    make arduino: builds data-chan as an arduino project

---
+++ Device/Bootstrap/main.c
@@ -0,0 +1,150 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2016.
+
+  dean [at] fourwalledcubicle [dot] com
+           www.lufa-lib.org
+*/
+
+/*
+  Copyright 2016  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaims all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Main source file for the Bulk Vendor demo. This file contains the main tasks of the demo and
+ *  is responsible for the initial application hardware configuration.
+ */
+
+#define  INCLUDE_FROM_BULKVENDOR_C
+#include <BulkVendor.h>
+#include <datachan.h>
+#include <Custom.h>
+
+static uint8_t ReceivedData[VENDOR_IO_EPSIZE];
+
+#ifndef ARDUINOLIB
+int main()
+{
+	main_setup();
+
+	for (;;) main_loop();
+}
+#endif // ARDUINOLIB
+
+void main_setup(void)
+{
+	SetupHardware();
+
+	GlobalInterruptEnable();
+}
+
+void main_loop(void)
+{
+		USB_USBTask();
+
+		memset(ReceivedData, 0x00, sizeof(ReceivedData));
+
+		Endpoint_SelectEndpoint(VENDOR_OUT_EPADDR);
+		if (Endpoint_IsOUTReceived())
+		{
+			Endpoint_Read_Stream_LE(ReceivedData, VENDOR_IO_EPSIZE, NULL);
+			Endpoint_ClearOUT();
+
+			// custom algorithm to parse request data
+			datachan_process_report(ReceivedData);
+
+			// do the main job (user defined)
+			MainRoutine();
+
+			// custom algorithm to generate measure/response data
+			datachan_generate_report(ReceivedData);
+
+			Endpoint_SelectEndpoint(VENDOR_IN_EPADDR);
+			Endpoint_Write_Stream_LE(ReceivedData, VENDOR_IO_EPSIZE, NULL);
+			Endpoint_ClearIN();
+		}
+}
+
+/** Configures the bare minimum hardware to setup the USB. */
+void SetupHardware(void)
+{
+#if (ARCH == ARCH_AVR8)
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
+#elif (ARCH == ARCH_XMEGA)
+	/* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
+	XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
+	XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
+
+	/* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
+	XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
+	XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
+
+	PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
+#endif
+
+	/* USB Hardware Initialization */
+	USB_Init();
+
+  /* Protocol initialization */
+  datachan_init();
+}
+
+/** Event handler for the USB_Connect event. This indicates that the device is enumerating via the status LEDs. */
+void EVENT_USB_Device_Connect(void)
+{
+	Event_Connected();
+}
+
+/** Event handler for the USB_Disconnect event. This indicates that the device is no longer connected to a host via
+ *  the status LEDs.
+ */
+void EVENT_USB_Device_Disconnect(void)
+{
+	datachan_sudden_disconnection();
+	Event_Disconnected();
+}
+
+/** Event handler for the USB_ConfigurationChanged event. This is fired when the host set the current configuration
+ *  of the USB device after enumeration - the device endpoints are configured.
+ */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+	bool ConfigSuccess = false;
+
+	/* Setup Vendor Data Endpoints */
+	ConfigSuccess &= Endpoint_ConfigureEndpoint(VENDOR_IN_EPADDR,  EP_TYPE_BULK, VENDOR_IO_EPSIZE, 1);
+	ConfigSuccess &= Endpoint_ConfigureEndpoint(VENDOR_OUT_EPADDR, EP_TYPE_BULK, VENDOR_IO_EPSIZE, 1);
+}
+
+/** Event handler for the USB_ControlRequest event. This is used to catch and process control requests sent to
+ *  the device from the USB host before passing along unhandled control requests to the library for processing
+ *  internally.
+ */
+void EVENT_USB_Device_ControlRequest(void)
+{
+	// Process vendor specific control requests here
+}
