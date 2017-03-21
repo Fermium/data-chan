f4622ec7f3e0e26f5c5e53f8a3b4925564f1851b
Davide Bortolami <d@fermiumlabs.com>
2017-03-21 13:59:42 +0100

    Working wercker trigger

--- scripts/post-build-hook.py
+++ scripts/post-build-hook.py
@@ -17,30 +17,35 @@ if os.environ.get('TRAVIS_PULL_REQUEST', "false")  != "false":
 
 # Get the current repository commit hash
 repo = git.Repo(search_parent_directories=True)
-sha = repo.head.object.hexsha
+hash = repo.head.object.hexhash.encode("ascii")
 
-
-#try if head is not detached
-try:
-    branch = repo.active_branch.name 
-except TypeError:
-    # apparently we are on a detached head, probably beacause we're building from a CI
-    # in this case, pick the first non-empty branch name from the ENV variables the CI provides
-    CIRepoENVVAr = []
-    CIRepoENVVAr.append(os.environ.get('TRAVIS_BRANCH', ""))
-    CIRepoENVVAr.append(os.environ.get('APPVEYOR_REPO_BRANCH', ""))
-    CIRepoENVVAr.append(os.environ.get('WERCKER_GIT_BRANCH', ""))
+def getBranch():
+    "Get the branch using git or CI environmental variables"
     
-    #Pick the first non-empty string
-    def get_nonempty(list_of_strings):
-        for s in list_of_strings:
-            if s:
-                return s
-    
-    branch = get_nonempty(CIRepoENVVAr)
+    #try if head is not detached
+    try:
+        branch = repo.active_branch.name 
+        return branch
+    except TypeError:
+        # apparently we are on a detached head, probably beacause we're building from a CI
+        # in this case, pick the first non-empty branch name from the ENV variables the CI provides
+        CIRepoENVVAr = []
+        CIRepoENVVAr.append(os.environ.get('TRAVIS_BRANCH', ""))
+        CIRepoENVVAr.append(os.environ.get('APPVEYOR_REPO_BRANCH', ""))
+        CIRepoENVVAr.append(os.environ.get('WERCKER_GIT_BRANCH', ""))
+        
+        #Pick the first non-empty string
+        def get_nonempty(list_of_strings):
+            for s in list_of_strings:
+                if s:
+                    return s
+        
+        branch = get_nonempty(CIRepoENVVAr)
+        return branch
+        
         
     
-print("SHA of this commit is " + sha + " on branch " + branch)
+print("hash of this commit is " + hash + " on branch " + getBranch())
 
 # Find the file to upload
 fileToUpload = glob.glob('Host/libDataChan.*')[0]
@@ -56,7 +61,7 @@ if os.environ.get('AWS_ACCESS_KEY_ID', "") == "" or os.environ.get('AWS_SECRET_A
 s3conn = tinys3.Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
 f = open(fileToUpload,'rb')
 bucket = os.environ.get('AWS_DESTINATION_BUCKET', "data-chan-js-binaries") 
-s3conn.upload(sha + "/" + fileToUpload.replace("Host/", ""),f, bucket )
+s3conn.upload(hash + "/" + fileToUpload.replace("Host/", ""),f, bucket )
 
 
 # if we are not on wercker
@@ -68,13 +73,20 @@ if os.environ.get('WERCKER_MAIN_PIPELINE_STARTED', "") == "":
         #endpoint to run (or re-run) a pipeline
         url = 'https://app.wercker.com/api/v3/runs'
         #authorization headers
-        headers = { 'Authorization':'Bearer ' + os.environ['WERCKER_TOKEN'] }
+        headers = { 'Authorization': 'Bearer ' + os.environ['WERCKER_TOKEN']}
         
         #get the pipeline id from environmental variables, fallback on default if not found
-        a = {"pipelineId": os.environ.get('WERCKER_DESTINATION_PIPELINE', "5855662ab7a7370100caf8fd"), 
-            "branch": branch, 
-            "commitHash": sha }
-        r = requests.get(url, headers=headers, verify=True, params=a)
+        payload = {"pipelineId": os.environ.get('WERCKER_DESTINATION_PIPELINE', "5855662ab7a7370100caf8fd"), 
+            "branch": getBranch(), 
+            "commitHash": hash }
+        r = requests.post(url, headers=headers, json=payload)
+        
+        if r.status_code == 200:
+            print("Wercker build triggered")
+        else:
+            print("Build not triggered, response code " + str(r.status_code))
+        print()
+
     else:
         print("We're not on Wercker but no WERCKER_TOKEN was found, so we can't trigger a new pipeline. Exiting.")
         sys.exit(0)
